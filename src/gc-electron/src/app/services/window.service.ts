29362bc83375c4e63dd4a2cebfd2f7b575f0c962
Isaac <idvidrine@gmail.com>
2024-06-11 02:49:16 -0400

    comments

--- src/gc-electron/src/app/services/window.service.ts
+++ src/gc-electron/src/app/services/window.service.ts
@@ -3,9 +3,6 @@ const share: any = (global as any).share;
 const WindowService = () => {
   share.ipcMain.on("window_minimize", (_: any) => {
     console.log("Minimizing");
-    // TODO: this method of finding the main window is definitely broken...
-    //      there is no gaurantee that the main window will be first in this array when a second window is open...
-    //      this is preventing us from opening the Knowledge Graph in its own window
     const mainWindow = share.BrowserWindow.getAllWindows()[0];
     mainWindow?.minimize();
   });
